3fd434cc807c3b26b798649d0d7caa5f552168ae
edelciofaria <63810656+edelciofaria@users.noreply.github.com>
2022-09-01 13:56:46 -0300

    Inserindo funcao ZERAR e PAUSAR

--- script_timer.js
+++ script_timer.js
@@ -6,13 +6,12 @@ var hora = document.getElementById("hora");
 var minuto = document.getElementById("min");
 var segundo = document.getElementById("seg");
 
-hora.innerHTML = "00";
-minuto.innerHTML = " : 00";
-segundo.innerHTML = " : 00";
+var relogio;
+zerar();
 
 function start(){
-    setTimeout(start, 1000);
     ss++;
+    relogio = setTimeout(start, 1000);
     if (ss<=59){
         mostraTempo(ss,mm,hh);
     }else{
@@ -30,6 +29,18 @@ function start(){
             }
         }
     }
+}
+function pause(){
+    clearTimeout(relogio);
+}
+function zerar(){
+    pause();
+    ss=0;
+    mm=0;
+    hh=0;
+    hora.innerHTML = "00";
+    minuto.innerHTML = " : 00";
+    segundo.innerHTML = " : 00";
 }
 function mostraTempo(s,m,h){
     //checando segundo <10
